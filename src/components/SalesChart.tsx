e72a7b9b05fa258b34ab005ca932d3f7a546cc72
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2024-01-29 23:43:23 -0300

    feat: rechart pt.2 (#0414)

--- src/components/SalesChart.tsx
+++ src/components/SalesChart.tsx
@@ -1,41 +1,49 @@
-import React from "react";
 import { ISale } from "../contexts/DataContext";
 import {
   LineChart,
   XAxis,
   Tooltip,
-  CartesianGrid,
   Line,
   YAxis,
   ResponsiveContainer,
   Legend,
 } from "recharts";
 
-const dataChart = [
-  {
-    data: "2024-01-03",
-    pago: 3000,
-    processando: 2000,
-    falha: 1500,
-  },
-  {
-    data: "2024-01-04",
-    pago: 6000,
-    processando: 2500,
-    falha: 1000,
-  },
-  {
-    data: "2024-01-54",
-    pago: 1000,
-    processando: 3500,
-    falha: 900,
-  },
-];
+type IDaySale = {
+  data: string;
+  pago: number;
+  processando: number;
+  falha: number;
+};
+
+const transformData = (data: ISale[]): IDaySale[] => {
+  const days = data.reduce((acc: { [key: string]: IDaySale }, item) => {
+    const day = item.data.split(" ")[0];
+
+    if (!acc[day]) {
+      acc[day] = {
+        data: day,
+        pago: 0,
+        falha: 0,
+        processando: 0,
+      };
+    }
+
+    acc[day][item.status] += item.preco;
+
+    return acc;
+  }, {});
+
+  return Object.values(days).map((dia) => ({
+    ...dia,
+    data: dia.data.substring(5),
+  }));
+};
 
 const SalesChart = ({ data }: { data: ISale[] }) => {
   return (
     <ResponsiveContainer width="99%" height={400}>
-      <LineChart data={dataChart}>
+      <LineChart data={transformData(data)}>
         <XAxis dataKey="data" />
         <YAxis />
         <Tooltip />
